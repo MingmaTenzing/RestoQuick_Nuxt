8712b147064421807a233a4541ad15550e7c7896
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-10-29 09:28:51 +0800

    initial commit - a full stack restaurant managment system

---
+++ app/pages/test.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+
+const data = ref('hello world')
+data.value = "not hello world"
+</script>
+
+
+<template>
+
+
+<p class=" font-bold bg-amber-400">{{ data }}</p>
+</template>
