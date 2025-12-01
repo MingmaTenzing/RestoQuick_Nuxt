4d8ee22df7a3664152de265e585446fcf68ed9f9
MingmaTenzing <pasangsherpa689@gmail.com>
2025-12-01 12:40:50 +0800

    checking custom tool calling

--- server/api/vapi-booking-tool/index.post.ts
+++ server/api/vapi-booking-tool/index.post.ts
@@ -1,6 +1,18 @@
+import { Vapi } from "@vapi-ai/server-sdk";
+
 export default defineEventHandler(async (event) => {
-    
-const params = param
+  const VAPI_TOOL_CALL: Vapi.ServerMessageToolCalls = await readBody(event);
+  console.log(VAPI_TOOL_CALL);
 
+  const response: Vapi.ServerMessageResponseToolCalls = {
+    results: [
+      {
+        name: VAPI_TOOL_CALL.toolCallList[0].function.name,
+        toolCallId: VAPI_TOOL_CALL.call!.id,
+        result: "Booking is confirmed",
+      },
+    ],
+  };
 
-})
+  return response;
+});
