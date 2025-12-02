0d450695df6e5f9f958a8e381289a475fdbe82c4
MingmaTenzing <pasangsherpa689@gmail.com>
2025-12-02 14:53:06 +0800

    hope it works

--- server/api/vapi-booking-tool/index.post.ts
+++ server/api/vapi-booking-tool/index.post.ts
@@ -1,54 +1,55 @@
 import { Vapi } from "@vapi-ai/server-sdk";
+import { create } from "node:domain";
 
 export default defineEventHandler(async (event) => {
-  // const VAPI_TOOL_CALL: Vapi.ServerMessageToolCalls = await readBody(event);
-  // console.log(VAPI_TOOL_CALL);
+  let args;
 
-  // let parsedArgs = null;
-  // const prisma = usePrisma();
-  // const body = await readBody(event);
-
-  // const VAPI_TOOL_REQUEST: Vapi.ServerMessageToolCalls = body.message;
-
-  // if (VAPI_TOOL_REQUEST) {
-  //   parsedArgs = JSON.parse(
-  //     VAPI_TOOL_REQUEST.toolCallList[0].function.arguments
-  //   );
-  // } else {
-  //   throw createError({
-  //     statusCode: 400,
-  //     statusMessage: "No body provided from VAPI",
-  //   });
-  // }
-
-  // const create_booking = await prisma.booking.create({
-  //   data: {
-  //     bookingTime: parsedArgs.bookingTime,
-  //     guestCount: parsedArgs.guestCount,
-  //     customerName: parsedArgs.customerName,
-  //     customerPhone: parsedArgs.customerPhone,
-  //     specialRequest: parsedArgs.specialRequest,
-  //   },
-  // });
-
-  // if (create_booking) {
-  //   return {
-  //     results: [
-  //       {
-  //         toolCallId: VAPI_TOOL_REQUEST.toolCallList[0].id,
-  //         result: `Booking has been made successfully `,
-  //       },
-  //     ],
-  //   };
-  // } else {
-  //   throw createError({
-  //     status: 500,
-  //     statusMessage: "Internal error creating booking",
-  //   });
-  // }
-
+  const prisma = usePrisma();
   const body = await readBody(event);
+  const toolCall = body.toolCalls[0];
 
-  console.log(body);
-  return body;
+  if (!toolCall) {
+    throw createError({
+      status: 400,
+      statusMessage: "bad request no too call provided",
+    });
+  }
+
+  const rawArgs = toolCall.function.arguments;
+  try {
+    args = JSON.parse(rawArgs);
+  } catch (error) {
+    throw createError({
+      status: 500,
+      statusMessage: "cannot parse the arguments",
+    });
+  }
+
+  console.log(args);
+
+  const create_booking = await prisma.booking.create({
+    data: {
+      bookingTime: args.bookingTime,
+      guestCount: args.guestCount,
+      customerName: args.customerName,
+      customerPhone: args.customerPhone,
+      specialRequest: args.specialRequest,
+    },
+  });
+
+  if (create_booking) {
+    return {
+      results: [
+        {
+          toolCallId: toolCall.toolCallList[0].id,
+          result: `Booking has been made successfully `,
+        },
+      ],
+    };
+  } else {
+    throw createError({
+      status: 500,
+      statusMessage: "Internal error creating booking",
+    });
+  }
 });
