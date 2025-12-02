900c7e6bb271fff4f2b588580b1a6d44e7dd4398
MingmaTenzing <pasangsherpa689@gmail.com>
2025-12-02 12:01:05 +0800

    updated the post request format for vapi booking -needs checkings if it working or not

--- server/api/vapi-booking-tool/index.post.ts
+++ server/api/vapi-booking-tool/index.post.ts
@@ -1,20 +1,49 @@
 import { Vapi } from "@vapi-ai/server-sdk";
-import { read } from "fs";
 
 export default defineEventHandler(async (event) => {
   // const VAPI_TOOL_CALL: Vapi.ServerMessageToolCalls = await readBody(event);
   // console.log(VAPI_TOOL_CALL);
 
+  let parsedArgs = null;
+  const prisma = usePrisma();
   const body = await readBody(event);
-  console.log(body);
-  return body;
-  // const response: Vapi.ServerMessageResponseToolCalls = {
-  //   results: [
-  //     {
-  //       name: VAPI_TOOL_CALL.toolCallList[0].function.name,
-  //       toolCallId: VAPI_TOOL_CALL.call!.id,
-  //       result: "Booking is confirmed",
-  //     },
-  //   ],
-  // };
+
+  const VAPI_TOOL_REQUEST: Vapi.ServerMessageToolCalls = body.message;
+
+  if (VAPI_TOOL_REQUEST) {
+    parsedArgs = JSON.parse(
+      VAPI_TOOL_REQUEST.toolCallList[0].function.arguments
+    );
+  } else {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "No body provided from VAPI",
+    });
+  }
+
+  const create_booking = await prisma.booking.create({
+    data: {
+      bookingTime: parsedArgs.bookingTime,
+      guestCount: parsedArgs.guestCount,
+      customerName: parsedArgs.customerName,
+      customerPhone: parsedArgs.customerPhone,
+      specialRequest: parsedArgs.specialRequest,
+    },
+  });
+
+  if (create_booking) {
+    return {
+      results: [
+        {
+          toolCallId: VAPI_TOOL_REQUEST.toolCallList[0].id,
+          result: `Booking has been made successfully `,
+        },
+      ],
+    };
+  } else {
+    throw createError({
+      status: 500,
+      statusMessage: "Internal error creating booking",
+    });
+  }
 });
