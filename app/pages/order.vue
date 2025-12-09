e2730342cc880a8e00024ef0af8ca00c44ab1fac
MingmaTenzing <pasangsherpa689@gmail.com>
2025-12-09 10:26:17 +0800

    the orders page is in progress - initial template is done - now next step will be working with the cart state - then work with stripe and database

--- app/pages/order.vue
+++ app/pages/order.vue
@@ -1,6 +1,9 @@
 <script lang="ts" setup>
 
-const {data} = useFetch('/api/menu-items')
+const { data } = useFetch('/api/menu-items')
+
+const show_cart = ref(true)
+
 const categories = [
     { id: "all", name: "All", icon: "🍽️" },
     { id: "appetizers", name: "Starters", icon: "🥗" },
@@ -11,7 +14,7 @@ const categories = [
 </script>
 
 <template >
-<main class="w-full p-4 space-y-6">
+<main class="w-full p-4 space-y-6 h-screen ">
 
     <section class=" flex space-x-2 items-center ">
         <img src="../assets/images/RestroMate.png"  class="invert w-20 not-dark:invert-0"></img>
@@ -37,7 +40,7 @@ const categories = [
     </section>
 
 
- <section class=" space-y-4">
+ <section class=" space-y-4 overflow-y-scroll h-[500px]">
 
 
     
@@ -53,6 +56,9 @@ const categories = [
                         <span class=" bg-accent p-2 rounded-lg font-semibold"> ${{ menu.price }}</span>
                     </div>
                     <p class=" text-muted-foreground font-light text-sm">{{ menu.description }}</p>
+
+
+                    <!-- add or minus the items number button -->
                     <div class=" flex items-center justify-between">
                         <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
 
@@ -79,13 +85,245 @@ const categories = [
 
 
 
-<!-- view cart -->
 
-<section>
 
+
+
+
+<!-- the position is set the fixed  -->
+<section class="w-full   fixed bottom-0 left-0">
+
+<!-- order summary popup when clicked on view cart -->
+    <div v-if="show_cart" class="h-[500px] bg-card p-4 rounded-t-xl">
+
+
+        <!-- order summary header -->
+        <div class=" mb-4 flex justify-between ">
+
+            
+            <div class=" flex flex-col">
+                <span class="font-bold text-2xl">Order Summary</span>
+                <span class=" font-light text-muted-foreground text-sm">Table Unknown</span>
+
+            </div>
+            <div>
+                <i class=" pi pi-times"></i>
+            </div>
+
+        </div>
+
+
+
+        <!-- food items cart list -->
+
+        <section class="space-y-4 h-[400px] overflow-y-scroll">
+
+
+            <!-- food item -->
+            <div class="flex border-b pb-4 justify-between">
+                <div class=" flex items-center gap-2 ">
+                    <NuxtImg src="https://foodish-api.com/images/pizza/pizza40.jpg" width="80" class="rounded-lg " />
+                    <div class=" space-y-2">
+                        <div class=" font-semibold">Margereta Pizza</div>
+                        <div class="font-light text-sm text-muted-foreground">$12.99 each</div>
+                   
+    
+                        <!-- add or minus the items number button -->
+                        <div class=" flex items-center gap-4">
+                            <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">-</span>
+                            </div>
+                            <div class="flex gap-1 items-center">
+    
+                                <span>2</span>
+                                <span class=" font-light text-muted-foreground text-sm">in cart</span>
+                            </div>
+    <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">+</span>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+    
+    
+                <!-- total -->
+                <div class=" flex flex-col gap-4 justify-between items-end">
+                     <i class=" pi pi-trash text-destructive"></i>
+                    <span class=" font-bold ">$40.50</span>
+    
+                </div>
+            </div>
+    
+            <!-- food item -->
+            <div class="flex border-b pb-4 justify-between">
+                <div class=" flex items-center gap-2 ">
+                    <NuxtImg src="https://foodish-api.com/images/pizza/pizza40.jpg" width="80" class="rounded-lg " />
+                    <div class=" space-y-2">
+                        <div class=" font-semibold">Margereta Pizza</div>
+                        <div class="font-light text-sm text-muted-foreground">$12.99 each</div>
+                   
+    
+                        <!-- add or minus the items number button -->
+                        <div class=" flex items-center gap-4">
+                            <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">-</span>
+                            </div>
+                            <div class="flex gap-1 items-center">
+    
+                                <span>2</span>
+                                <span class=" font-light text-muted-foreground text-sm">in cart</span>
+                            </div>
+    <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">+</span>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+    
+    
+                <!-- total -->
+                <div class=" flex flex-col gap-4 justify-between items-end">
+                     <i class=" pi pi-trash text-destructive"></i>
+                    <span class=" font-bold ">$40.50</span>
+    
+                </div>
+            </div>
+    
+            <!-- food item -->
+            <div class="flex border-b pb-4 justify-between">
+                <div class=" flex items-center gap-2 ">
+                    <NuxtImg src="https://foodish-api.com/images/pizza/pizza40.jpg" width="80" class="rounded-lg " />
+                    <div class=" space-y-2">
+                        <div class=" font-semibold">Margereta Pizza</div>
+                        <div class="font-light text-sm text-muted-foreground">$12.99 each</div>
+                   
+    
+                        <!-- add or minus the items number button -->
+                        <div class=" flex items-center gap-4">
+                            <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">-</span>
+                            </div>
+                            <div class="flex gap-1 items-center">
+    
+                                <span>2</span>
+                                <span class=" font-light text-muted-foreground text-sm">in cart</span>
+                            </div>
+    <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">+</span>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+    
+    
+                <!-- total -->
+                <div class=" flex flex-col gap-4 justify-between items-end">
+                     <i class=" pi pi-trash text-destructive"></i>
+                    <span class=" font-bold ">$40.50</span>
+    
+                </div>
+            </div>
+            <!-- food item -->
+            <div class="flex border-b pb-4 justify-between">
+                <div class=" flex items-center gap-2 ">
+                    <NuxtImg src="https://foodish-api.com/images/pizza/pizza40.jpg" width="80" class="rounded-lg " />
+                    <div class=" space-y-2">
+                        <div class=" font-semibold">Margereta Pizza</div>
+                        <div class="font-light text-sm text-muted-foreground">$12.99 each</div>
+                   
+    
+                        <!-- add or minus the items number button -->
+                        <div class=" flex items-center gap-4">
+                            <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">-</span>
+                            </div>
+                            <div class="flex gap-1 items-center">
+    
+                                <span>2</span>
+                                <span class=" font-light text-muted-foreground text-sm">in cart</span>
+                            </div>
+    <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">+</span>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+    
+    
+                <!-- total -->
+                <div class=" flex flex-col gap-4 justify-between items-end">
+                     <i class=" pi pi-trash text-destructive"></i>
+                    <span class=" font-bold ">$40.50</span>
+    
+                </div>
+            </div>
+            <!-- food item -->
+            <div class="flex border-b pb-4 justify-between">
+                <div class=" flex items-center gap-2 ">
+                    <NuxtImg src="https://foodish-api.com/images/pizza/pizza40.jpg" width="80" class="rounded-lg " />
+                    <div class=" space-y-2">
+                        <div class=" font-semibold">Margereta Pizza</div>
+                        <div class="font-light text-sm text-muted-foreground">$12.99 each</div>
+                   
+    
+                        <!-- add or minus the items number button -->
+                        <div class=" flex items-center gap-4">
+                            <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">-</span>
+                            </div>
+                            <div class="flex gap-1 items-center">
+    
+                                <span>2</span>
+                                <span class=" font-light text-muted-foreground text-sm">in cart</span>
+                            </div>
+    <div class=" rounded-lg border p-2 w-10 h-10 flex justify-center items-center">
+    
+                                <span class=" ">+</span>
+                            </div>
+                        </div>
+    
+                    </div>
+                </div>
+    
+    
+                <!-- total -->
+                <div class=" flex flex-col gap-4 justify-between items-end">
+                     <i class=" pi pi-trash text-destructive"></i>
+                    <span class=" font-bold ">$40.50</span>
+    
+                </div>
+            </div>
+        </section>
+
+
+    </div>
+
+    <!-- view cart button-->
+
+    <div @click="show_cart = !show_cart" class="bg-green-600 w-full text-white  p-4 rounded-lg flex space-x-2 items-center justify-center">
+        <i class=" pi pi-shopping-cart"></i>
+        <div>View Cart</div>
+        <div>(2 item)</div>
+        <div class="font-bold">$65.45</div>
+
+    </div>
+
+    
 
 </section>
-
-
 </main>
+
 </template>
