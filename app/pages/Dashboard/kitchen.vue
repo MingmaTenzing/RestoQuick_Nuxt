85695c969de52645d1fbd1030b09d3ec797941c9
MingmaTenzing <pasangsherpa689@gmail.com>
2025-12-30 00:32:21 +0800

    status header done

--- app/pages/Dashboard/kitchen.vue
+++ app/pages/Dashboard/kitchen.vue
@@ -1,10 +1,71 @@
 <script lang="ts" setup>
+import { ref, computed } from 'vue'
 
+definePageMeta({
+    layout: 'dashboard-layout'
+})
 
 </script>
+
+
 <template>
 
+    <main>
+            <!-- header -->
+        <section class=" space-y-4">
+ <div>
+     <h2 class="text-2xl md:text-6xl"> Kitchen Display </h2>
+     <p class=" text-sm lg:text-base text-muted-foreground">Manage incoming order and preparation status</p>
+ 
 
+ </div>
+            <!-- stats -->
+            <section class=" flex justify-around gap-4">
+                    <!-- pending orders -->
+                <div class="p-4 border bg-card rounded-lg h-40  justify-between flex flex-col w-full  ">
+                    <div class=" flex gap-2 text-lg text-amber-600 items-center">
+                        <p class=" ">Pending Orders </p>
+                        <i class=" pi pi-clock"></i>
 
+                    </div>
+        
+                    <div class=" flex flex-col">
+                        <span class=" text-4xl font-bold text-amber-600">7</span>
+                        <p class=" text-muted-foreground font-light text-sm">Currently in Kitchen</p>
+                    </div>
+                    
+                </div>
+        
+                <!-- completed orders -->
+                <div class="p-4 border bg-card rounded-lg h-40  justify-between flex flex-col w-full ">
+                     <div class=" flex gap-2 text-lg text-green-600 items-center">
+                        <p class=" ">Completed Orders </p>
+                        <i class=" pi pi-check-circle"></i>
 
-    </template>
+                    </div>
+                    <div class=" flex flex-col">
+                        <span class=" text-4xl font-bold text-green-600">78</span>
+                        <p class=" text-muted-foreground font-light text-sm">Completed by kitchen</p>
+                    </div>
+                    
+        
+                </div>
+        
+        
+                <!-- total orders -->
+                <div class="p-4 border bg-card rounded-lg h-40  justify-between flex flex-col w-full ">
+                    <p class=" text-lg">Total Orders (Today) </p>
+        
+                    <div class=" flex flex-col">
+                        <span class=" text-4xl font-bold">121</span>
+                        <p class=" text-muted-foreground font-light text-sm">Total for {{ new Date(Date.now()) }}</p>
+                    </div>
+                    
+                </div>
+              
+        
+            </section>
+        </section>
+
+    </main>
+</template>
