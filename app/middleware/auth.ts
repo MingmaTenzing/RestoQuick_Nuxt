edb7516cf41cb60ed8799db67881e31d1312a1c3
MingmaTenzing <pasangsherpa689@gmail.com>
2026-04-03 13:45:58 +0800

    removed is loaded

--- app/middleware/auth.ts
+++ app/middleware/auth.ts
@@ -1,9 +1,6 @@
 export default defineNuxtRouteMiddleware(() => {
   const { isSignedIn, userId, isLoaded } = useAuth();
 
-  if (!isLoaded.value) {
-    return;
-  }
   if (!isSignedIn.value && !userId.value) {
     return navigateTo("/sign-in");
   }
