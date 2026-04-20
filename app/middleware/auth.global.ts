ea250f04d0536af9d189836418705aa423580d0a
MingmaTenzing <pasangsherpa689@gmail.com>
2026-04-20 17:33:26 +0800

    updates

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,6 +2,7 @@
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default defineNuxtRouteMiddleware((to) => {
+<<<<<<< HEAD
   // Use the `useAuth()` composable to access the `isSignedIn` property
   const { isSignedIn, has, orgRole, orgId } = useAuth();
   const { user } = useUser();
@@ -10,10 +11,27 @@ export default defineNuxtRouteMiddleware((to) => {
   console.log("im auth middleware checking auth");
   console.log(`user is signed in? ${isSignedIn.value}`);
   console.log(user.value);
+=======
+  const toast = useToast();
+  const { isSignedIn, orgId } = useAuth();
+  if (!isProtectedRoute(to)) {
+    return;
+  }
 
-  // Check if the user is not signed in and is trying to access a protected route
-  // If so, redirect them to the sign-in page
-  if (!isSignedIn.value && isProtectedRoute(to)) {
+  const runtimeConfig = useRuntimeConfig();
+  const allowedOrgId = runtimeConfig.public.CLERK_ORG_ID;
+>>>>>>> 8fbe68f (sign in redirects to dashboard and now view demo also works as it signs it in a demo account automatically)
+
+  if (!isSignedIn.value) {
     return navigateTo("/sign-in");
+  }
+
+  if (allowedOrgId && orgId.value !== allowedOrgId) {
+    toast.error({
+      title: "Access Denied",
+      message: "You do not have permission to access this page.",
+    });
+
+    return navigateTo("/");
   }
 });
