c5198626861f90cdc97957e47c527689442023c1
MingmaTenzing <pasangsherpa689@gmail.com>
2026-04-20 17:32:12 +0800

    sign in redirects to dashboard and now view demo also works as it signs it in a demo account automatically

--- app/middleware/auth.global.ts
+++ app/middleware/auth.global.ts
@@ -2,26 +2,14 @@
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
 export default defineNuxtRouteMiddleware((to) => {
-<<<<<<< HEAD
-  // Use the `useAuth()` composable to access the `isSignedIn` property
-  const { isSignedIn, has, orgRole, orgId } = useAuth();
-  const { user } = useUser();
-  console.log(orgRole.value);
-
-  console.log("im auth middleware checking auth");
-  console.log(`user is signed in? ${isSignedIn.value}`);
-  console.log(user.value);
-=======
-  const toast = useToast();
-  const { isSignedIn, orgId } = useAuth();
   if (!isProtectedRoute(to)) {
     return;
   }
 
+  const toast = useToast();
+  const { isSignedIn, orgId } = useAuth();
   const runtimeConfig = useRuntimeConfig();
   const allowedOrgId = runtimeConfig.public.CLERK_ORG_ID;
->>>>>>> 8fbe68f (sign in redirects to dashboard and now view demo also works as it signs it in a demo account automatically)
-
   if (!isSignedIn.value) {
     return navigateTo("/sign-in");
   }
